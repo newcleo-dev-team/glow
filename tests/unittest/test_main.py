55ad8868bf561b9b4fc1bd9f0a89e6f8c54ff4bd
Davide Manzione <davide.manzione@newcleo.com>
2025-07-25 15:15:08 +0200

    [024_test_suite], issue #24, Revised test case in 'test_main.py' module
    The test for assessing the correctness of the 'analyse_and_generate_tdt' function has been revised in its structure by suddividing it into private methods. The test case docstring has been revised as well.

--- tests/unittest/test_main.py
+++ tests/unittest/test_main.py
@@ -10,18 +10,27 @@ from glow.main import analyse_and_generate_tdt
 from glow.support.types import EDGE_NAME_VS_TYPE, EdgeType, GeometryType, \
     LatticeGeometryType, PropertyType
 
-# Patch sys.path to allow imports from the main module
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'glow')))
 
-
-
-class TestLattice(unittest.TestCase):
+class TestMainFunction(unittest.TestCase):
     """
-    Test case for verifying that the characteristics of the geometry layout
+    Test case for verifying the correct implementation of the function
+    `analyse_and_generate_tdt` declared in the `main.py` module.
+    The test verifies that the characteristics of the geometry layout
     are correctly extracted and the output TDT is generated.
 
     Attributes
     ----------
+    lattice : Lattice
+        The `Lattice` instance whose geometric characteristics are
+        exported to file.
+    file_name : str
+        The path name of the output TDT file.
+    geom_type : GeometryType
+        The type of geometry for the lattice's cells, as element of the
+        `GeometryType` enumeration.
+    prop_type : PropertyType
+        The type of property assigned to the lattice's regions, as element
+        of the `PropertyType` enumeration.
     """
     def setUp(self):
         """
@@ -59,37 +68,59 @@ class TestLattice(unittest.TestCase):
                                  property_type=self.prop_type)
 
         # Verify that the output TDT file has been generated
-        self.assertTrue(
-            os.path.exists(self.file_name)
-        )
+        self.assertTrue(os.path.exists(self.file_name))
         # Read the content of the TDT file and verify the correct data is
         # present
         with open(self.file_name, "r") as f:
             content = f.read()
 
-        #Verify the header section
-        regions_nb = len(self.lattice.regions)
-        edges_nb = 4
-        self.assertIn(
-            f"{LatticeGeometryType.RECTANGLE_TRAN.value:5d},{0:5d}, " + \
-            f"{regions_nb:5d}, {edges_nb:5d}, " +\
-            f"{1:5d},{regions_nb:5d}, {0:5d}, {1:5d}", content)
-        # Verify the values for impressions and precisions
-        self.assertIn(f"{0:5d}  {0:6d}  1", content)
-        self.assertIn(f"{1e-5:7E}   {1e-5:7E}", content)
-
+        # Verify the header section
+        self.__assess_header_section(content)
         # Verify the regions section
-        regions_indices = ""
-        for i in range(0, len(self.lattice.regions), 12):
-            regions_indices += ",".join(f"{(j+1):4d}"
-                for j, _ in enumerate(self.lattice.regions[i:i+12])) + ",\n"
-        # Remove the last comma
-        regions_indices = \
-            "*   flux region number per geometry region (mesh)\n" + \
-            regions_indices[0:-2] + "\n"
-        self.assertIn(regions_indices, content)
-
+        self.__assess_regions_section(content)
         # Verify the edges section (only segment-type edges are present)
+        self.__assess_edges_section(content)
+        # Verify the boundaries section
+        self.__assess_boundaries_section(content)
+        # Verify the properties section
+        self.assertIn(f"# {1:2d} - {'MAT'}", content)
+        self.assertIn(f"  {1}", content)
+
+    def __assess_boundaries_section(self, content) -> None:
+        """
+        Method that tests that the boundaries section, contained in the
+        input string, contains the expected data.
+
+        Parameters
+        ----------
+        content : str
+            The content string to search for the expected data in.
+        """
+        # List storing the boundary axes for each edge
+        boundaries = [
+            (0, 1.0, 0.0),
+            (-1.0, 0.0, 90.0),
+            (0, -1.0, 0.0),
+            (1.0, 0.0, 90.0),
+        ]
+        self.assertIn(f"  0, {len(boundaries)}, 0", content)
+        self.assertIn("* albedo\n  1.0", content) # FIXME have attribute
+        for i, bc in enumerate(boundaries):
+            self.assertIn("* type  number of elements\n  2, 1", content)
+            self.assertIn(f"*   elements\n{i+1}", content)
+            self.assertIn("* tx, ty, angle", content)
+            self.assertIn(f"{bc[0]:6f} {bc[1]:6f} {bc[2]:6f}", content)
+
+    def __assess_edges_section(self, content) -> None:
+        """
+        Method that tests that the edges section, contained in the
+        input string, contains the expected data.
+
+        Parameters
+        ----------
+        content : str
+            The content string to search for the expected data in.
+        """
         # List declaring the edge's XY coordinates of the starting point
         # and the dx, dy values
         edges = [
@@ -106,21 +137,43 @@ class TestLattice(unittest.TestCase):
                 f"  {edge[0]:6f}, {edge[1]:6f}, {edge[2]:6f}, {edge[3]:6f}",
                 content)
 
-        # Verify the boundaries section
-        self.assertIn(f"  0, {len(edges)}, 0", content)
-        self.assertIn("* albedo\n  1.0", content) # FIXME have attribute
-        boundaries = [
-            (0, 1.0, 0.0),
-            (-1.0, 0.0, 90.0),
-            (0, -1.0, 0.0),
-            (1.0, 0.0, 90.0),
-        ]
-        for i, bc in enumerate(boundaries):
-            self.assertIn("* type  number of elements\n  2, 1", content)
-            self.assertIn(f"*   elements\n{i+1}", content)
-            self.assertIn("* tx, ty, angle", content)
-            self.assertIn(f"{bc[0]:6f} {bc[1]:6f} {bc[2]:6f}", content)
+    def __assess_header_section(self, content) -> None:
+        """
+        Method that tests that the header section, contained in the
+        input string, contains the expected data.
 
-        # Verify the properties section
-        self.assertIn(f"# {1:2d} - {'MAT'}", content)
-        self.assertIn(f"  {1}", content)
+        Parameters
+        ----------
+        content : str
+            The content string to search for the expected data in.
+        """
+        regions_nb = len(self.lattice.regions)
+        edges_nb = 4
+        self.assertIn(
+            f"{LatticeGeometryType.RECTANGLE_TRAN.value:5d},{0:5d}, " + \
+            f"{regions_nb:5d}, {edges_nb:5d}, " +\
+            f"{1:5d},{regions_nb:5d}, {0:5d}, {1:5d}", content)
+        # Verify the values for impressions and precisions
+        self.assertIn(f"{0:5d}  {0:6d}  1", content)
+        self.assertIn(f"{1e-5:7E}   {1e-5:7E}", content)
+
+    def __assess_regions_section(self, content) -> None:
+        """
+        Method that tests that the regions section, contained in the
+        input string, contains the expected data.
+
+        Parameters
+        ----------
+        content : str
+            The content string to search for the expected data in.
+        """
+        regions_indices = ""
+        for i in range(0, len(self.lattice.regions), 12):
+            regions_indices += ",".join(f"{(j+1):4d}"
+                for j, _ in enumerate(self.lattice.regions[i:i+12])) + ",\n"
+        # Remove the last comma
+        regions_indices = \
+            "*   flux region number per geometry region (mesh)\n" + \
+            regions_indices[0:-2] + "\n"
+        self.assertIn(regions_indices, content)
+
